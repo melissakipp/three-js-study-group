f6109e46d8981a8a218b5309afd775d6e96315f5
melissakipp <melissakipp.az@gmail.com>
2024-08-08 07:18:21 -0700

    First draft of hunted house {issue} Door

--- src/projects/basic/eleven.js
+++ src/projects/basic/eleven.js
@@ -9,7 +9,52 @@ const canvas = document.querySelector('canvas.materials');
 
 // sceneThree
 const sceneThree = new THREE.Scene();
-sceneThree.background = new THREE.Color(0xf8f8f8);
+sceneThree.background = new THREE.Color(0xf8f8f8); //323232
+
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader();
+
+const doorColorTexture = textureLoader.load('/textures/door/color.jpg');
+const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg');
+const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg');
+const doorHeightTexture = textureLoader.load('/textures/door/height.jpg');
+const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg');
+const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg');
+const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg');
+const matcapTexture = textureLoader.load('/textures/matcaps/8.png');
+const gradientTexture = textureLoader.load('/textures/gradients/5.jpg');
+console.log(doorColorTexture);
+doorColorTexture.colorSpace = THREE.SRGBColorSpace;
+matcapTexture.colorSpace = THREE.SRGBColorSpace;
+
+/**
+ * Objects
+ */
+// MeshBasicMaterial
+const material = new THREE.MeshBasicMaterial();
+material.map = doorColorTexture;
+
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  material
+);
+sphere.position.x = -1.5;
+
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry(1, 1, 100, 100),
+  material
+);
+
+const torus = new THREE.Mesh(
+  new THREE.TorusGeometry(0.3, 0.2, 16, 32),
+  material
+);
+torus.position.x = 1.5;
+
+sceneThree.add(sphere, plane, torus);
 
 /**
  * Sizes
@@ -42,9 +87,9 @@ window.addEventListener('resize', () => {
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
-camera.position.x = 1;
-camera.position.y = 1;
-camera.position.z = 2;
+camera.position.x = 1.75;
+camera.position.y = 1.75;
+camera.position.z = 2.75;
 sceneThree.add(camera);
 
 // Controls
@@ -67,6 +112,15 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  // Update objects
+  sphere.rotation.y = 0.1 * elapsedTime
+  plane.rotation.y = 0.1 * elapsedTime
+  torus.rotation.y = 0.1 * elapsedTime
+
+  sphere.rotation.x = - 0.15 * elapsedTime
+  plane.rotation.x = - 0.15 * elapsedTime
+  torus.rotation.x = - 0.15 * elapsedTime
 
   // Update controls
   controls.update();
